21b6a77f4ed6760cfa327a1e05547210e357a410
Ivayla Radkova <84006683+IvaylaR-125@users.noreply.github.com>
2021-12-08 09:59:16 +0200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(400, 400);
+  background(0);
+}
+
+function draw() {
+  translate(width / 2, height / 2);
+  
+  v = p5.Vector.random2D();
+  v.mult(random(30, 100));
+  
+  strokeWeight(4);
+  stroke(255, 70);
+  line(0, 0, v.x, v.y);
+}
